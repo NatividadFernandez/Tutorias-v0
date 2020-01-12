29624010dc751b861f7cfac473ed259d09f4d3f1
Natii <Natii@DESKTOP-7K17A8A>
2020-01-12 19:08:36 +0100

    Creación clase Sesión, métodos, atributos y test pasados correctamente.

---
+++ src/main/java/org/iesalandalus/programacion/tutorias/mvc/modelo/negocio/Sesiones.java
@@ -0,0 +1,144 @@
+package org.iesalandalus.programacion.tutorias.mvc.modelo.negocio;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Sesion;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.Tutoria;
+
+public class Sesiones {
+
+	private Sesion[] coleccionSesiones;
+	private int capacidad;
+	private int tamano;
+
+	// Constructor con parámetros
+	public Sesiones(int capacidad) {
+		if (capacidad <= 0) {
+			throw new IllegalArgumentException("ERROR: La capacidad debe ser mayor que cero.");
+		}
+		this.capacidad = capacidad;
+		coleccionSesiones = new Sesion[capacidad];
+		tamano = 0;
+	}
+
+	// Getters
+	public Sesion[] get() {
+		return coleccionSesiones;
+	}
+
+	// Copia profunda sesiones
+	private Sesion[] copiaProfundaProfesores() {
+		Sesion[] copiaSesion = new Sesion[coleccionSesiones.length];
+		for (int i = 0; i < coleccionSesiones.length && coleccionSesiones[i] != null; i++) {
+			copiaSesion[i] = new Sesion(coleccionSesiones[i]);
+		}
+		return copiaSesion;
+	}
+
+	public Sesion[] get(Tutoria tutoria) {
+		Sesion[] sesionTutoria = new Sesion[tamano];
+		int tutorias = 0;
+		for (int i = 0; !tamanoSuperado(i); i++) {
+			if (coleccionSesiones[i].getTutoria().equals(tutoria)) {
+				sesionTutoria[tutorias++] = new Sesion(coleccionSesiones[i]);
+			}
+		}
+		return sesionTutoria;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public int getTamano() {
+		return tamano;
+	}
+
+	// Insertar sesion
+	public void insertar(Sesion sesion) throws OperationNotSupportedException {
+
+		if (sesion == null) {
+			throw new NullPointerException("ERROR: No se puede insertar una sesión nula.");
+		}
+
+		int indice = buscarIndice(sesion);
+
+		if (capacidadSuperada(indice)) {
+			throw new OperationNotSupportedException("ERROR: No se aceptan más sesiones.");
+		}
+
+		if (tamanoSuperado(indice)) {
+			coleccionSesiones[indice] = new Sesion(sesion);
+			tamano += 1;
+		} else {
+			throw new OperationNotSupportedException("ERROR: Ya existe una sesión con esa fecha.");
+		}
+
+	}
+
+	// Buscamos el indice
+	public int buscarIndice(Sesion sesion) {
+		int indice = 0;
+		boolean sesionEncontrada = false;
+		while (!tamanoSuperado(indice) && !sesionEncontrada) {
+			if (coleccionSesiones[indice].equals(sesion)) {
+				sesionEncontrada = true;
+			} else {
+				indice++;
+			}
+		}
+		return indice;
+	}
+
+	// Comprobamos tamaño
+	private boolean tamanoSuperado(int indice) {
+		return indice >= tamano;
+	}
+
+	// Comprobamos capacidad
+	private boolean capacidadSuperada(int indice) {
+		return indice >= capacidad;
+	}
+
+	// Buscar alumnos
+	public Sesion buscar(Sesion sesion) {
+		if (sesion == null) {
+			throw new IllegalArgumentException("ERROR: No se puede buscar una sesión nula.");
+		}
+
+		int indice = buscarIndice(sesion);
+		if (!tamanoSuperado(indice)) {
+			return new Sesion(coleccionSesiones[indice]);
+		} else {
+			return null;
+		}
+
+	}
+
+	// Desplazar posicion a la izquierda
+	private void desplazarUnaPosicionHaciaIzquierda(int indice) {
+
+		for (int i = indice; !tamanoSuperado(i); i++) {
+			coleccionSesiones[i] = coleccionSesiones[i + 1];
+
+		}
+		tamano--;
+	}
+
+	// Borrar alumnos
+	public void borrar(Sesion sesion) throws OperationNotSupportedException {
+
+		if (sesion == null) {
+			throw new IllegalArgumentException("ERROR: No se puede borrar una sesión nula.");
+		}
+
+		int indice = buscarIndice(sesion);
+
+		if (!tamanoSuperado(indice)) {
+			desplazarUnaPosicionHaciaIzquierda(indice);
+		} else {
+			throw new OperationNotSupportedException("ERROR: No existe ninguna sesión con esa fecha.");
+		}
+	}
+
+}
--- src/test/java/org/iesalandalus/programacion/tutorias/mvc/modelo/AllTests.java
+++ src/test/java/org/iesalandalus/programacion/tutorias/mvc/modelo/AllTests.java
@@ -8,15 +8,15 @@ import org.iesalandalus.programacion.tutorias.mvc.modelo.dominio.TutoriaTest;
 import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.AlumnosTest;
 //import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.CitasTest;
 import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.ProfesoresTest;
-/*import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.SesionesTest;
-import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.TutoriasTest;*/
+import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.SesionesTest;
+/*import org.iesalandalus.programacion.tutorias.mvc.modelo.negocio.TutoriasTest;*/
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AlumnoTest.class, CitaTest.class, ProfesorTest.class, SesionTest.class, TutoriaTest.class,
-	AlumnosTest.class,/* CitasTest.class,*/ ProfesoresTest.class/*, SesionesTest.class, TutoriasTest.class,
+	AlumnosTest.class,/* CitasTest.class,*/ ProfesoresTest.class, SesionesTest.class/*, TutoriasTest.class,
 	ModeloTest.class */})
 public class AllTests {
 
